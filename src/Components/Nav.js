5d898fcc0b5896fb7bbd5d9fc012b9c34d05303f
rohittiwari833 <rohittiwari7262@gmail.com>
2022-07-21 00:17:22 +0530

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FiMenu } from 'react-icons/fi'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <h3>ganesh</h3>
+      <div className='menu'>
+        <FiMenu className='icon' />
+      </div>
+    </div>
+  )
+}
+
+export default Nav
