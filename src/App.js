f708d419e17a792baad85e3c80ff161b8e9cd45e
rohittiwari833 <rohittiwari7262@gmail.com>
2022-07-22 23:23:29 +0530

    add video cam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Data } from './Pages/Home/Plantinfo'
-// import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router'
 import Home from './Pages/Home/Home'
 import Nav from './Components/Nav'
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
 import Plant from './Components/PlantImages/Plant'
 
 function App() {
-  // let Navigate = useNavigate()
-  // useEffect(() => {
-  //   Navigate('/detect')
-  // }, [Navigate])
+  let Navigate = useNavigate()
+  useEffect(() => {
+    Navigate('/detect')
+  }, [])
   return (
     <div className='App'>
       <Nav />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/detect' element={<Home />} />
         {Data.map((item) => (
-          <Route path={item.route} element={<Plant />} />
+          <Route
+            path={`detect/${item.route}`}
+            element={<Plant />}
+            key={item.id}
+          />
         ))}
       </Routes>
     </div>
--- src/Components/PlantImages/Plant.js
+++ src/Components/PlantImages/Plant.js
@@ -1,45 +1,53 @@
-import React, { useState, useRef, useCallback } from 'react'
-import Webcam from 'react-webcam'
+import React, { useEffect, useRef, useState } from 'react'
 import './Plant.css'
 
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  facingMode: 'user',
-}
-
-const WebcamCapture = () => {
+const CamCapture = () => {
+  const videoRef = useRef(null)
+  const photoRef = useRef(null)
   const [image, setImage] = useState()
-  const webcamRef = useRef(null)
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot()
-    setImage(imageSrc)
-  }, [webcamRef])
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef])
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then((stream) => {
+        let video = videoRef.current
+        video.srcObject = stream
+        video.play()
+      })
+      .catch((err) => {
+        console.error('error:', err)
+      })
+  }
+
+  const takePhoto = () => {
+    let photo = photoRef.current
+
+    const data = photo.toDataURL('image/jpeg')
+
+    console.warn(data)
+    setImage(data)
+  }
+
   return (
     <div className='container'>
-      <div className='cam'>
-        <Webcam
-          className='webcam'
-          audio={false}
-          height={400}
-          ref={webcamRef}
-          screenshotFormat='image/jpeg'
-          width={200}
-          videoConstraints={videoConstraints}
-        />
+      <div className='cam-video'>
+        <video ref={videoRef} className='player' />
       </div>
       <div className='btns'>
-        <button className='capture' onClick={capture}>
+        <button className='capture' onClick={takePhoto}>
           detect
         </button>
         <br />
         <p>OR</p>
-
         <button className='upload'>upload</button>
       </div>
-      <img src={image} alt='Screenshot' />
+      <img src={image} alt='screenshot'></img>
     </div>
   )
 }
 
-export default WebcamCapture
+export default CamCapture
